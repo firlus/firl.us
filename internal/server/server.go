5c553fa00ad0d5f20bdb603d3b48a9799c4c15dc
Michael Firlus <michael@4lus.de>
2020-11-20 18:15:22 +0100

    Renamed route

--- internal/server/server.go
+++ internal/server/server.go
@@ -15,7 +15,7 @@ func Setup(port string, storage store.Store) {
 	apiRoute := router.Group("/api")
 	router.GET("/:api", api.RedirectToShortcut)
 	apiRoute.POST("/shortcuts", api.ShortcutPost)
-	apiRoute.GET("/allshortcuts", api.AllShortcutsGet)
+	router.GET("/:api/:shortcuts", api.AllShortcutsGet)
 
 	// This route is different (due to Gin's annoying router.)
 	// Should look like that:
