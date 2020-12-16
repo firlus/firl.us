3b8132b84282d6c5ceebf8c4db5e6aa8877f83fc
Michael Firlus <michael@4lus.de>
2020-12-16 16:00:25 +0100

    ✨ [Admin-Panel] Introduced admin panel

--- web/js/app.js
+++ web/js/app.js
@@ -1,18 +1,36 @@
-import Vue from 'vue';
-import 'bootstrap';
+import Vue from "vue";
+import Router from "vue-router";
+import "bootstrap";
 
-import ShortcutList from './components/ShortcutList.vue';
-import ShortcutCreator from './components/ShortcutCreator.vue';
-import Shortcut from './components/Shortcut.vue';
+import ShortcutList from "./components/ShortcutList.vue";
+import ShortcutCreator from "./components/ShortcutCreator.vue";
+import Shortcut from "./components/Shortcut.vue";
+import Login from "./components/Login.vue";
 
-Vue.component('shortcut-list', ShortcutList);
-Vue.component('shortcut-creator', ShortcutCreator);
-Vue.component('shortcut', Shortcut);
+Vue.component("shortcut-list", ShortcutList);
+Vue.component("shortcut-creator", ShortcutCreator);
+Vue.component("shortcut", Shortcut);
+
+const router = new Router({
+  routes: [
+    {
+      path: "/",
+      name: "Login",
+      component: Login,
+    },
+    {
+      path: "/list",
+      name: "List",
+      component: ShortcutList,
+    },
+  ],
+});
 
 new Vue({
-    el: '#app',
-    components: {
-        ShortcutList,
-        Shortcut
-    }
-})
+  el: "#app",
+  router: router,
+  components: {
+    ShortcutList,
+    Shortcut,
+  },
+});
